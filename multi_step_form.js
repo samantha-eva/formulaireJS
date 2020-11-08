255036f106bdf8d8c9d67e2d4e2cadfb28dc94c0
MarcelBragance <bragance.m@hotmail.fr>
2020-11-08 19:56:37 +0100

    Vérification champs & dossier image

--- multi_step_form.js
+++ multi_step_form.js
@@ -1,3 +1,77 @@
+//******************************************
+//***FUNCTION VALIDATION FORM Page 1 ******
+//******************************************
+function verification(nom_controle) {
+    var longueur = document.getElementById(nom_controle).value.length;
+    var b_civilit=false;
+     //validation civilite//
+     if (nom_controle == 'civilite') {
+        if (document.getElementById(nom_controle).value > 0) {
+            document.getElementById(nom_controle).style.border = '#9FE855 1px solid';
+            b_civilit = true
+        }
+        else {
+            b_civilit = false
+            document.getElementById(nom_controle).style.border = '#CC3300 3px solid';
+            alert('veuillez selectioner votre civilité')
+        }
+        console.log(b_civilit);
+    }
+    //validation string nom & prénon //
+    else if (nom_controle == 'nom' || nom_controle == 'prenom') {
+        var letters = /^[A-Za-z]+$/;
+        if (!document.getElementById(nom_controle).value.match(letters)) {
+            document.getElementById(nom_controle).style.border = '#CC3300 3px solid';
+            alert("veuillez tapez des lettres");
+            b_nom = false;
+            b_prenom = false
+        }
+        else {
+            b_nom = true;
+            b_prenom = true;
+            document.getElementById(nom_controle).style.border = '#096A09 2px solid';
+        }
+    }
+    //validation email//
+    else if (nom_controle == 'email') {
+        if (document.getElementById(nom_controle).value.indexOf("@") == -1 || document.getElementById(nom_controle).value.indexOf(".") == -1) {
+            document.getElementById(nom_controle).style.border = '#CC3300 3px solid';
+            alert("veuillez saisir un email valid");
+            b_email = false
+        }
+        else {
+            b_email = true
+            document.getElementById(nom_controle).style.border = '#9FE855 1px solid';
+        }
+    }
+    //validation telephone//
+    else if (nom_controle == 'phone') {
+        var number =/^[0-9-+\s()]+$/;
+        if (longueur < 10 ||!document.getElementById(nom_controle).value.match(number)) {
+            document.getElementById(nom_controle).style.border = '#CC3300 3px solid';
+            alert("veuillez saisir un n°téléphone valid");
+            b_telephone = false
+        }
+        else {
+            b_telephone = true
+            document.getElementById(nom_controle).style.border = '#9FE855 2px solid';
+        }
+    }
+    //validation statut//
+    else if(nom_controle == 'statut'){
+        if(document.getElementById(nom_controle).value > 0){
+            b_statut = true;
+            document.getElementById(nom_controle).style.border = '#9FE855 2px solid';
+        }
+        else{
+            b_statut = false;
+            document.getElementById(nom_controle).style.border = '#CC3300 3px solid';
+            alert("veuillez selectioner un statut");
+        }
+    }
+}
+
+
 // fontion button suivant avec verification.
 function next_step1(event) {
     var count = 0;
@@ -15,7 +89,7 @@ function next_step1(event) {
         }
 
     if (count != 0  ) {
-    alert("*All Fields are mandatory*"); 
+    alert("*le formulaire n'est pas complet*"); 
     event.preventDefault();
     } else {
     return true;
